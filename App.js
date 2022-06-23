a0f3a19fe38a4eaea9bdb4b991aa7bf3ef98e5c0
WARLIBIA NASCIMENTO DA SILVA <202002460644@alunos.estacio.br>
2022-06-23 16:33:23 -0300

    ultimo

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react'
 import {NavigationContainer} from '@react-navigation/native'
 import { createStackNavigator} from '@react-navigation/stack'
 
-import Imc from './src/pages/Imc';
+import Home from './src/pages/Home';
 import Time from './src/pages/Time'; 
 
 const Stack = createStackNavigator();
@@ -11,8 +11,8 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Imc">
-          <Stack.Screen name="Imc" component={Imc} />
+      <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="Time" component={Time} />
       </Stack.Navigator>
     </NavigationContainer>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { View, Text, Button} from 'react-native'
+import { View, Text, Button } from 'react-native'
 
 export default function Imc ({ navigation }) {
   return (
     <View>
-      <Text>Imc</Text>
+      <Text>IMC CALCULATOR</Text>
+
       <Button
-      title="Time"
+      title="IMC CALCULATOR"
       onPress={ () => navigation.navigate ('Time')}
       />
     </View>
