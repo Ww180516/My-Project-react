f74a8641889c555b83f7c1b726f9a4fd25546f62
WARLIBIA NASCIMENTO DA SILVA <202002460644@alunos.estacio.br>
2022-06-23 18:33:36 -0300

    final

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, ImageBackground } from 'react-native'
 
-export default function Imc ({ navigation }) {
+export default function Home ({ navigation }) {
   return (
-    <View>
-      <Text>IMC CALCULATOR</Text>
+    <View style={{justifyContent:"center", alignItems:"center"}}>
+      <ImageBackground source={require('../../../assets/Welfare.png')}
+      style={{width:'100%', height:200}}>     
+    
 
+      </ImageBackground>
       <Button
       title="IMC CALCULATOR"
       onPress={ () => navigation.navigate ('Time')}
       />
     </View>
+  
   );
 }
